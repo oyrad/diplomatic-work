af9287691c439b4111768c2e5e9a1f0107fcd21f
Dario <dario.susanj2@gmail.com>
2023-10-11 23:54:55 +0200

    shorten functions

--- functions.py
+++ functions.py
@@ -22,21 +22,11 @@ def get_climatology_by_level(values, levels):
     for level in range(len(levels)):
         level_total = []
         for time_step in range(len(values)):
-            level_total.append(values[time_step][level][0][0])
-
-        climatology_by_level.append(np.mean(level_total))
-
-    return climatology_by_level
-
-
-def get_climatology_by_level2(values1, values2, levels):
-    climatology_by_level = []
-
-    for level in range(len(levels)):
-        level_total = []
-        for time_step in range(len(values1)):
-            level_total.append(
-                (values1[time_step][level][0][0] + values2[time_step][level][0][0]) / 2)
+            if (len(values) == 2):
+                level_total.append(
+                    (values[0][time_step][level][0][0] + values[1][time_step][level][0][0]) / 2)
+            else:
+                level_total.append(values[time_step][level][0][0])
 
         climatology_by_level.append(np.mean(level_total))
 
@@ -52,37 +42,12 @@ def get_trend(values, levels):
         sum = 0
         counter = 0
 
-        for time_step in range(len(values)):
-            sum += values[time_step][level][0][0]
-
-            if time_step < 12 and counter == 11 or time_step > 12 and counter == 12:
-                anomalies_by_year.append(
-                    sum / 12 - climatology_by_level[level])
-                sum = 0
-                counter = 0
-
-            counter += 1
-
-        years = np.arange(1940, 2023, 1).reshape(-1, 1)
-
-        model = LinearRegression().fit(years, anomalies_by_year)
-        slopes.append(model.coef_ * 10)
-
-    return slopes
-
-
-def get_trend2(values1, values2, levels):
-    slopes = []
-    climatology_by_level = get_climatology_by_level2(values1, values2, levels)
-
-    for level in range(len(levels)):
-        anomalies_by_year = []
-        sum = 0
-        counter = 0
-
-        for time_step in range(len(values1)):
-            sum = sum + ((values1[time_step][level][0][0] +
-                         values2[time_step][level][0][0]) / 2)
+        for time_step in range(996):
+            if (len(values) == 2):
+                sum = sum + ((values[0][time_step][level][0][0] +
+                              values[1][time_step][level][0][0]) / 2)
+            else:
+                sum += values[time_step][level][0][0]
 
             if time_step < 12 and counter == 11 or time_step > 12 and counter == 12:
                 anomalies_by_year.append(
