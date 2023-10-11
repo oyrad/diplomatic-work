b344fd2da1eca3fb4f2413af61445b6bd5b2dd62
Dario <dario.susanj2@gmail.com>
2023-10-11 23:42:42 +0200

    add day mean graph

--- functions.py
+++ functions.py
@@ -29,6 +29,20 @@ def get_climatology_by_level(values, levels):
     return climatology_by_level
 
 
+def get_climatology_by_level2(values1, values2, levels):
+    climatology_by_level = []
+
+    for level in range(len(levels)):
+        level_total = []
+        for time_step in range(len(values1)):
+            level_total.append(
+                (values1[time_step][level][0][0] + values2[time_step][level][0][0]) / 2)
+
+        climatology_by_level.append(np.mean(level_total))
+
+    return climatology_by_level
+
+
 def get_trend(values, levels):
     slopes = []
     climatology_by_level = get_climatology_by_level(values, levels)
@@ -42,7 +56,37 @@ def get_trend(values, levels):
             sum += values[time_step][level][0][0]
 
             if time_step < 12 and counter == 11 or time_step > 12 and counter == 12:
-                anomalies_by_year.append(sum / 12 - climatology_by_level[level])
+                anomalies_by_year.append(
+                    sum / 12 - climatology_by_level[level])
+                sum = 0
+                counter = 0
+
+            counter += 1
+
+        years = np.arange(1940, 2023, 1).reshape(-1, 1)
+
+        model = LinearRegression().fit(years, anomalies_by_year)
+        slopes.append(model.coef_ * 10)
+
+    return slopes
+
+
+def get_trend2(values1, values2, levels):
+    slopes = []
+    climatology_by_level = get_climatology_by_level2(values1, values2, levels)
+
+    for level in range(len(levels)):
+        anomalies_by_year = []
+        sum = 0
+        counter = 0
+
+        for time_step in range(len(values1)):
+            sum = sum + ((values1[time_step][level][0][0] +
+                         values2[time_step][level][0][0]) / 2)
+
+            if time_step < 12 and counter == 11 or time_step > 12 and counter == 12:
+                anomalies_by_year.append(
+                    sum / 12 - climatology_by_level[level])
                 sum = 0
                 counter = 0
 
--- graphs.py
+++ graphs.py
@@ -42,6 +42,37 @@ def trend(temp, rel, spec, levels, title):
     ax[2].set_ylabel("Tlak [hPa]", fontsize=label_size)
 
 
+def trend2(temp1, temp2, rel1, rel2, spec1, spec2, levels, title):
+    dashed_line_color = "#888"
+
+    fig, ax = plt.subplots(1, 3, figsize=(20, 10))
+    fig.suptitle(title, fontsize=suptitle_size)
+
+    ax[0].semilogy(fn.get_trend2(temp1, temp2, levels), levels)
+    ax[0].invert_yaxis()
+    ax[0].axvline(0, color=dashed_line_color, linestyle="dashed")
+
+    ax[1].semilogy(fn.get_trend2(rel1, rel2, levels), levels)
+    ax[1].invert_yaxis()
+    ax[1].axvline(0, color=dashed_line_color, linestyle="dashed")
+
+    ax[2].semilogy(fn.get_trend2(spec1, spec2, levels), levels)
+    ax[2].invert_yaxis()
+    ax[2].axvline(0, color=dashed_line_color, linestyle="dashed")
+
+    ax[0].set_title("Temperatura", fontsize=title_size)
+    ax[0].set_xlabel(r"Trend [$^{\circ}$ C / 10 god]", fontsize=label_size)
+    ax[0].set_ylabel("Tlak [hPa]", fontsize=label_size)
+
+    ax[1].set_title("Relativna vlažnost", fontsize=title_size)
+    ax[1].set_xlabel(r"Trend [$\%$ / 10 god]", fontsize=label_size)
+    ax[1].set_ylabel("Tlak [hPa]", fontsize=label_size)
+
+    ax[2].set_title("Specifična vlažnost", fontsize=title_size)
+    ax[2].set_xlabel(r"Trend [gkg$^{-1}$ / 10 god]", fontsize=label_size)
+    ax[2].set_ylabel("Tlak [hPa]", fontsize=label_size)
+
+
 def hovmoeller(values, levels, cmap_levels, cmap_label, title):
     fig, ax = plt.subplots(1, 1, figsize=(16, 8))
 
